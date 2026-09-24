58a25f61d70bbd41ee642c6680629967699080c1
Oumaima <mahfoudeoumaima@gmail.com>
2026-09-24 16:25:17 +0100

    CHAPITRE 8

---
+++ Structurer l’application en composants/3. Structurer et vérifier l’API (AJAX)/frontend/app.js
@@ -0,0 +1,13 @@
+fetch('../backend/categories.php')
+.then(Response => Response.json())
+.then(categories => {
+    const liste = document.getElementById('categories');
+    categories.forEach(category =>  {
+        const li = document.createElement('li');
+        li.textContent = category.name;
+        liste.appendChild(li);
+    });
+})
+.catch(error => {
+    console.error("erreur : " , error);
+});
